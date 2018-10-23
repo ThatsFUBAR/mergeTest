4a7f941b00fe8bdaf1f08584946fbbaaf1e25f0a
Joe Christensen <jchristensen12@email.arizona.edu>
2018-10-23 15:56:01 -0700

    confilct in new-feature

--- src/Merges.java
+++ src/Merges.java
@@ -2,10 +2,9 @@
 public class Merges {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from the master branch");
+		System.out.println("Hello from the new feature brach");
 		
-		System.out.println("Hello from the new-feature branch");
-
+		System.out.println("Hello again from the new-feature branch");
 	}
 
 }
